0694c3639e7050320d08bd00b8c2cac6bb0a1977
Tonnie <vontonnie@gmail.com>
2022-10-19 16:08:56 +0300

    Add CatDabase Class

--- app/src/main/java/com/uxstate/catfactwithremotemediator/data/local/CatDatabase.kt
+++ app/src/main/java/com/uxstate/catfactwithremotemediator/data/local/CatDatabase.kt
@@ -2,7 +2,11 @@ package com.uxstate.catfactwithremotemediator.data.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.uxstate.catfactwithremotemediator.data.local.dao.CatDao
+import com.uxstate.catfactwithremotemediator.data.local.entity.CatFactEntity
 
-@Database()
+@Database(entities = [CatFactEntity::class], version = 1)
 abstract class CatDatabase: RoomDatabase() {
+
+    abstract val dao:CatDao
 }
