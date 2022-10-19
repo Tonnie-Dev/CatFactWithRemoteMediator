f15b64356cc3ccd737baddf168e5aeb4a223a507
Tonnie <vontonnie@gmail.com>
2022-10-19 16:54:19 +0300

    Add abstract val to the CatDatabase class

--- app/src/main/java/com/uxstate/catfactwithremotemediator/data/local/CatDatabase.kt
+++ app/src/main/java/com/uxstate/catfactwithremotemediator/data/local/CatDatabase.kt
@@ -3,10 +3,12 @@ package com.uxstate.catfactwithremotemediator.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.uxstate.catfactwithremotemediator.data.local.dao.CatDao
+import com.uxstate.catfactwithremotemediator.data.local.dao.RemoteKeysDao
 import com.uxstate.catfactwithremotemediator.data.local.entity.CatFactEntity
 
 @Database(entities = [CatFactEntity::class], version = 1)
 abstract class CatDatabase: RoomDatabase() {
 
-    abstract val dao:CatDao
+    abstract val factsDao:CatDao
+    abstract val keysDao:RemoteKeysDao
 }
