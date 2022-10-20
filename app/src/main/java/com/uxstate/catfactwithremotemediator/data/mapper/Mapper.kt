2845442fbf2035975cd6bb7bce2d1fdbc35c0f40
Tonnie <vontonnie@gmail.com>
2022-10-20 09:41:31 +0300

    Add CatFactDTO.toEntity() mapper extension function

--- app/src/main/java/com/uxstate/catfactwithremotemediator/data/mapper/Mapper.kt
+++ app/src/main/java/com/uxstate/catfactwithremotemediator/data/mapper/Mapper.kt
@@ -1,5 +1,6 @@
 package com.uxstate.catfactwithremotemediator.data.mapper
 
+import com.uxstate.catfactwithremotemediator.data.local.entity.CatFactsEntity
 import com.uxstate.catfactwithremotemediator.data.remote.dto.CatFactDTO
 import com.uxstate.catfactwithremotemediator.data.remote.dto.ApiResponseDTO
 import com.uxstate.catfactwithremotemediator.domain.model.ApiResponse
@@ -28,4 +29,9 @@ fun ApiResponseDTO.toModel(): ApiResponse {
     )
 }
 
-//response from model to dto
+//CatFactDTO to entity
+
+fun CatFactDTO.toEntity(): CatFactsEntity{
+
+    return CatFactsEntity(id = null, fact = this.fact, length = this.length)
+}
