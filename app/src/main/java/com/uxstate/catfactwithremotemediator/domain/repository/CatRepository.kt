64ad87eca7e20af9b335e2237860b4478ab2e2e2
Tonnie <vontonnie@gmail.com>
2022-10-20 12:15:04 +0300

    Add getStats() in the repository definition

--- app/src/main/java/com/uxstate/catfactwithremotemediator/domain/repository/CatRepository.kt
+++ app/src/main/java/com/uxstate/catfactwithremotemediator/domain/repository/CatRepository.kt
@@ -1,9 +1,12 @@
 package com.uxstate.catfactwithremotemediator.domain.repository
 
 import com.uxstate.catfactwithremotemediator.domain.model.ApiResponse
+import com.uxstate.catfactwithremotemediator.domain.model.Stats
+import kotlinx.coroutines.flow.Flow
 
 interface CatRepository {
 
 
    suspend fun getCatFacts(page:Int):ApiResponse
+   fun getStats():Flow<Stats>
 }
